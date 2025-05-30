f3e1d853881cff87d3f36208ca17e48084d03036
NABZX-nab <nabzx54@gmail.com>
2025-05-30 08:02:27 +0700

    add: new token by fhmii_15

--- data.js
+++ data.js
@@ -6,6 +6,7 @@
     "7813742099:AAGuYPFHElbP_6HaEaUxachfo_0TT4pBPUY",
     "8189787523:AAH95lplfZ1F_jH9Q3fRmugXt3UqJNR04kI",
     "<8030063427:AAEN-mxjryvo_uFi7aGK4DOW4rPCn-j4xc0_bot_telegram>",
-    "<8030063427:AAEN-mxjryvo_uFi7aGK4DOW4rPCn-j4xc0_fardan_botz>"
+    "<8030063427:AAEN-mxjryvo_uFi7aGK4DOW4rPCn-j4xc0_fardan_botz>",
+    "<8030063427:AAEN-mxjryvo_uFi7aGK4DOW4rPCn-j4xc0>"
   ]
 }
